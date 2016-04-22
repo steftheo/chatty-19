9178c12ed7f1c6898fc4afcbaa4ceebe5a57aff7
Stefan Theofilos <StefanTheofilos@Stefans-MacBook-Pro.local>
2016-04-21 22:17:22 -0500

    login-register-adapter route

---
+++ app/application/adapter.js
@@ -0,0 +1,9 @@
+import JSONAPIAdapter from 'ember-data/adapters/json-api';
+import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
+
+export default JSONAPIAdapter.extend(DataAdapterMixin, {
+  authorizer: `authorizer:application`,
+
+  host: `https://chatty-tn-api.herokuapp.com`,
+  namespace: `api`,
+});
--- app/router.js
+++ app/router.js
@@ -7,6 +7,10 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('chatter');
+  this.route('public', function() {
+    this.route('login');
+    this.route('register');
+  });
 });
 
 export default Router;
